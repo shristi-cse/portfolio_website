f7734dd5bb82c3d7cca62529c675e19fbfe13ed2
Shristi Raj <shristiraj2007@gmail.com>
2026-02-01 09:29:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,86 +2,12 @@ const openMeBtn = document.querySelector('[data-me-target]');
 const mePopup = document.querySelector('#Me');
 const closeBtn = document.querySelector('[data-close-button]');
 
-
 openMeBtn.addEventListener('click', () => {
     mePopup.classList.add('active');
 });
 
 closeBtn.addEventListener('click', () => {
     mePopup.classList.remove('active');
-});
-
-const popup = document.querySelector(".Me");
-const header = popup.querySelector(".header");
-
-const skillPopup = document.getElementById("Skills");
-const skillsHeader = skillPopup.querySelector(".skills-header");
-
-const contact = document.getElementById('contact');
-const linksheader = contact.querySelector('.links-header');
-
-
-let isDragging = false;
-let offsetX = 0;
-let offsetY = 0;
-
-header.addEventListener("mousedown", (e) => {
-    isDragging = true;
-    popup.classList.add("dragging");
-
-    offsetX = e.clientX - popup.offsetLeft;
-    offsetY = e.clientY - popup.offsetTop;
-});
-
-document.addEventListener("mousemove", (e) => {
-    if (!isDragging) return;
-
-    popup.style.left = e.clientX - offsetX + "px";
-    popup.style.top = e.clientY - offsetY + "px";
-    popup.style.transform = "none";
-});
-
-document.addEventListener("mouseup", () => {
-    isDragging = false;
-    popup.classList.remove("dragging");
-});
-
-skillsHeader.addEventListener("mousedown", (e) => {
-    isDragging = true
-    offsetX = e.clientX - skillPopup.offsetLeft;
-    offsetY = e.clientY - skillPopup.offsetTop;
-    skillPopup.classList.add("dragging");
-});
-
-document.addEventListener("mousemove",(e) => {
-    if (!isDragging) return;
-
-    skillsPopup.style.left = e.clientX - offsetX + "px";
-    skillPopup.style.top = e.clientY - offsetY + "px";
-});
-
-document.addEventListener("mouseup", () => {
-    isDragging = false;
-    skillPopup.classList.remove("dragging");
-});
-
-linksheader.addEventListener("mousedown", (e) => {
-    isDragging = true
-    offsetX = e.clientX - contact.offsetLeft;
-    offsetY = e.clientY - contact.offsetTop;
-    contact.classList.add("dragging");
-});
-
-document.addEventListener("mousemove",(e) => {
-    if (!isDragging) return;
-
-    contact.style.left = e.clientX - offsetX + "px";
-    contact.style.top = e.clientY - offsetY + "px";
-});
-
-document.addEventListener("mouseup", () => {
-    isDragging = false;
-    contact.classList.remove("dragging");
 });
 
 const openSkillsBtn = document.querySelector('[data-skills-target]');
@@ -110,6 +36,50 @@ closelinksBtn.addEventListener('click', () => {
     linksPopup.classList.remove('active');
 });
 
+function makeDraggable(popup, header) {
+    let newX = 0, newY = 0, startX = 0, startY = 0;
+
+   header.addEventListener('mousedown', mouseDown)
+
+  function mouseDown(e){
+    startX = e.clientX 
+    startY = e.clientY
+
+    document.addEventListener('mousemove', mouseMove)
+    document.addEventListener('mouseup', mouseUp)
+  }
+
+  function mouseMove(e){
+    newX = startX - e.clientX
+    newY = startY - e.clientY
+
+    startX = e.clientX
+    startY = e.clientY
+
+    popup.style.top = (popup.offsetTop - newY) + 'px'
+    popup.style.left = (popup.offsetLeft - newX) + 'px'
+  }
+
+  function mouseUp(){
+    document.removeEventListener('mousemove', mouseMove)
+  }
+
+}
+
+
+const Me = document.getElementById('Me');
+const Meheader = Me.querySelector('.header');
+
+const Skills = document.getElementById('Skills');
+const skillsheader = Skills.querySelector('.skills-header');
+
+const contact = document.getElementById('contact');
+const linksheader = contact.querySelector('.links-header');
+
+makeDraggable(Me, Meheader);
+makeDraggable(Skills, skillsheader);
+makeDraggable(contact, linksheader);
+
 document.addEventListener("DOMContentLoaded", () => {
     const notice = document.getElementById("mobileNotice");
     const closeb = document.getElementById("closeNotice");
@@ -127,4 +97,5 @@ icons.forEach(icon => {
     const delay = Math.random() * 4;
 
     icon.style.animationDelay = `${delay}s`;
+
 });
